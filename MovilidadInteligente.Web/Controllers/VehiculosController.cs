f6db5a472325acd69302ae6c7c0b8fa96d55ce8e
Zauzetua <yisuschuy12345@hotmail.com>
2026-04-29 19:05:38 -0700

    Añadido controller de Vehiculos, vacio

---
+++ MovilidadInteligente.Web/Controllers/VehiculosController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovilidadInteligente.Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VehiculosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
